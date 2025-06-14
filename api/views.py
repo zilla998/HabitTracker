bb3fad32df75a85e2ce0997ef0ee5453a6d89209
zilla998 <coldssielenssams@gmail.com>
2025-06-14 12:13:46 +0300

    fix RegisterView

--- api/views.py
+++ api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions, status
+from rest_framework import permissions, status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
@@ -38,7 +38,10 @@ class HabitRecordViewSet(ModelViewSet):
 
 
 
-class RegisterView(APIView):
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
     def post(self, request):
         serializer =RegisterSerializer(data=request.data)
         if serializer.is_valid():
